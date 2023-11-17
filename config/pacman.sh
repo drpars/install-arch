5ad4bc3be3ee508d0a082dc243da6adf32945894
M. Izzet Saglam <m.izzetsaglam@gmail.com>
2023-11-17 22:03:50 +0300

    updated installer

--- config/pacman.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# pacman options
-sed -i '/#Color/s/^#//g' /etc/pacman.conf
-sed -i '/#VerbosePkgLists/s/^#//g' /etc/pacman.conf
-sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 3/g' /etc/pacman.conf
-# Acivate multilib repository
-sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-pacman -Sy
